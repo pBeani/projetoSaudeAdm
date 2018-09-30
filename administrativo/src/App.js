41ec344d9fe69fc7292ea59bb55468a78df9993d
Pedro Beani <bunnyzones25@gmail.com>
2018-09-30 16:14:52 -0300

    Configurando projeto

---
+++ administrativo/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+import classes from './App.css';
+
+class App extends Component {
+
+  render() {
+    return null
+  }
+}
+
+export default App;
